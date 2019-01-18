27fb2b2fca27b9ef02c96d43b6c32129215b7755
Gerhard Mandl <mangem13@htl-kaindorf.ac.at>
2019-01-18 11:48:33 +0100

    update

---
+++ ue02_typescript/ka/main.js
@@ -0,0 +1,4 @@
+console.log('Hallo, guten Morgen');
+var jahr = 2018;
+var monat = "September";
+console.log(monat + ' ' + jahr);
---
+++ ue04MyWebsite/src/app/cars.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-cars',
+    templateUrl: './cars.component.html'
+})
+export class CarsComponent {
+}
